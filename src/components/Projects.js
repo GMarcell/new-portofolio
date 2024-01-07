0d13877316f07845b73cb29316e839c91bfd487d
malik <malik@maven.co.id>
2024-01-07 22:37:06 +0700

    add vue project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,67 +1,73 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import instagramClone from "../assets/img/instagramClone.PNG"
-import netflixClone from "../assets/img/netflixClone.PNG"
-import photoGalery from "../assets/img/photoGalery.PNG"
-import reactTodo from "../assets/img/reactToDo.PNG"
-import pokedex from "../assets/img/Pokedex.PNG"
-import vueTodoApp from "../assets/img/vue-todo-app.png"
-import frvrhngry from "../assets/img/frvrhngry.png"
+import instagramClone from "../assets/img/instagramClone.PNG";
+import netflixClone from "../assets/img/netflixClone.PNG";
+import photoGalery from "../assets/img/photoGalery.PNG";
+import reactTodo from "../assets/img/reactToDo.PNG";
+import pokedex from "../assets/img/Pokedex.PNG";
+import vueTodoApp from "../assets/img/vue-todo-app.png";
+import frvrhngry from "../assets/img/frvrhngry.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import nuxtmovieapp from "../assets/img/nuxt-movie-app.jpg"
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import nuxtmovieapp from "../assets/img/nuxt-movie-app.jpg";
+import vueExpenseTracker from "../assets/img/vue-expense-tracker.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
-
   const projects = [
     {
       title: "Instagram Clone",
       description: "ReactJS & Firebase",
       imgUrl: instagramClone,
-      Url: "https://instagram-clone-d1422.firebaseapp.com/"
+      Url: "https://instagram-clone-d1422.firebaseapp.com/",
     },
     {
       title: "Netflix Clone",
       description: "ReactJS & Firebase",
       imgUrl: netflixClone,
-      Url: "https://netflix-clone-5a379.firebaseapp.com/"
+      Url: "https://netflix-clone-5a379.firebaseapp.com/",
     },
     {
       title: "Photo Galery",
       description: "ReactJS & Firebase",
       imgUrl: photoGalery,
-      Url: "https://clone-instagram-6f601.firebaseapp.com/"
+      Url: "https://clone-instagram-6f601.firebaseapp.com/",
     },
     {
       title: "React ToDo",
       description: "ReactJS & Firebase",
       imgUrl: reactTodo,
-      Url: "https://fir-todo-c24d8.web.app/"
+      Url: "https://fir-todo-c24d8.web.app/",
     },
     {
       title: "Pokedex",
       description: "ReactJS & PokeAPI",
       imgUrl: pokedex,
-      Url: "https://simple-pokedex-new.netlify.app/"
+      Url: "https://simple-pokedex-new.netlify.app/",
     },
     {
       title: "Todo App",
       description: "VueJS",
       imgUrl: vueTodoApp,
-      Url: "https://benevolent-sawine-8d2d61.netlify.app"
+      Url: "https://benevolent-sawine-8d2d61.netlify.app",
     },
     {
       title: "frvrhngry",
       description: "ReactJS",
       imgUrl: frvrhngry,
-      Url: "https://frvrhngry.netlify.app/"
+      Url: "https://frvrhngry.netlify.app/",
     },
     {
       title: "Movie App",
       description: "NuxtJS & TMDB API",
       imgUrl: nuxtmovieapp,
-      Url: "https://nuxtjs-movie-app.netlify.app/"
+      Url: "https://nuxtjs-movie-app.netlify.app/",
+    },
+    {
+      title: "Expense Tracker",
+      description: "VueJS",
+      imgUrl: vueExpenseTracker,
+      Url: "https://vue-money-tracker.netlify.app/",
     },
   ];
 
@@ -71,12 +77,16 @@ export const Projects = () => {
         <Row>
           <Col size={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>These are some of the projects I have developed</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  {/* <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <p>These are some of the projects I have developed</p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    {/* <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
                       <Nav.Link eventKey="first">ReactJS</Nav.Link>
                     </Nav.Item>
@@ -87,35 +97,34 @@ export const Projects = () => {
                       <Nav.Link eventKey="third">Tab 3</Nav.Link>
                     </Nav.Item>
                   </Nav> */}
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    {/* <Tab.Pane eventKey="second">
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      {/* <Tab.Pane eventKey="second">
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                     </Tab.Pane> */}
-                    {/* <Tab.Pane eventKey="third">
+                      {/* <Tab.Pane eventKey="third">
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                     </Tab.Pane> */}
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
       <img className="background-image-right" src={colorSharp2}></img>
     </section>
-  )
-}
+  );
+};
